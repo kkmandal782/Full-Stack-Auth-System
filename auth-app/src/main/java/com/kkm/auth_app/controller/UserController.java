925ac58b545da28909a94364278b20513ce518c5
kamlesh mandal <kkmandalmbi1999@gmail.com>
2026-06-20 23:25:28 +0530

    Remove unwanted code from UserController

--- auth-app/src/main/java/com/kkm/auth_app/controller/UserController.java
+++ auth-app/src/main/java/com/kkm/auth_app/controller/UserController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//TODO ** I need to make multi instance -- youtube
 @RestController
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
